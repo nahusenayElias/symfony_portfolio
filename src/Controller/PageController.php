fed8fd88120586a764ea6272257410bc63449b68
nahusenayElias <eliasars@yahoo.com>
2025-04-19 22:50:15 +0300

    feat: Init commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('page/home.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request): Response
+    {
+        // Create the form
+        $form = $this->createForm(\App\Form\ContactType::class);
+        $form->handleRequest($request);
+
+        $success = false;
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Here you could send an email or save the message
+            $success = true;
+            // Optionally: $this->addFlash('success', 'Thank you for your message!');
+        }
+
+        return $this->render('page/contact.html.twig', [
+            'contactForm' => $form->createView(),
+            'success' => $success,
+        ]);
+    }
+}
