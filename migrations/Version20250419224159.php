ebae0ec7365e8691fb120374690b4ec65191b787
nahusenayElias <eliasars@yahoo.com>
2025-04-20 23:00:45 +0300

    feat: Add Blog page

---
+++ migrations/Version20250419224159.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250419224159 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE blog_post (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, author VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN blog_post.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN messenger_messages.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN messenger_messages.available_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN messenger_messages.delivered_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+                BEGIN
+                    PERFORM pg_notify('messenger_messages', NEW.queue_name::text);
+                    RETURN NEW;
+                END;
+            $$ LANGUAGE plpgsql;
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE blog_post
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
+        SQL);
+    }
+}
